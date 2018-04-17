3bc95a1acd1ea06526885b1736c3d1ad802711a8
David Stap <dd.stap@gmail.com>
2018-04-17 14:58:23 +0200

    list of lists

--- project1/load_corpus.py
+++ project1/load_corpus.py
@@ -1,8 +1,8 @@
 ## Loads training corpus
 def load_train(fpath):
     with open(fpath+'/training/hansards.36.2.e','r') as f:
-        english = f.read().splitlines()
+        english = [x.split() for x in f.read().splitlines()]
 
     with open(fpath+'/training/hansards.36.2.f','r') as f:
-        french = f.read().splitlines()
+        french = [x.split() for x in f.read().splitlines()]
     return [english,french]
